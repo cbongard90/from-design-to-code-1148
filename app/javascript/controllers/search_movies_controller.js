3e5386d95f8b3ccdb3354b00ed7badd61f1fe810
Christian Bongard <christian.bongard90@gmail.com>
2023-06-10 10:47:19 +0400

    added AJAX search

--- app/javascript/controllers/search_movies_controller.js
+++ app/javascript/controllers/search_movies_controller.js
@@ -11,6 +11,11 @@ export default class extends Controller {
   }
 
   update() {
-    console.log(this.inputTarget.value)
+    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
+    fetch(url, {headers: {"Accept": "text/plain"}})
+      .then(response => response.text())
+      .then((data) => {
+        this.listTarget.outerHTML = data
+      })
   }
 }
